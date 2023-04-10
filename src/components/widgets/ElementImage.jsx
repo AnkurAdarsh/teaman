c6e592795b70792cd508e9fe47d1c263c6b18823
Ankur Adarsh <ankuradarsh123@gmail.com>
2023-04-10 05:43:07 +0530

    Added location

--- src/components/widgets/ElementImage.jsx
+++ src/components/widgets/ElementImage.jsx
@@ -8,8 +8,9 @@ const ElementImage = (props) => {
                 <img src={props.image} className="brightness-50 opacity-90" alt="under-design"/>
             </div>
             <div className='p-5 grid place-items-center'>
-                <label className='text-white font-Berk pt-14 md:pt-24 lg:pt-20 pb-5 text-7xl lg:text-8xl'>{props.text}</label>
-                <img src={underdesign} alt="under-design" className=''/>
+                <label className='text-white font-Berk text-4xl pt-6 sm:pt-14 lg:pt-14 pb-5 md:text-left'>{props.contact}</label>
+                <label className='text-white font-Berk text-6xl md:text-7xl pb-10 lg:pb-10  '>{props.text}</label>
+                <img src={underdesign} alt="under-design" className='w-44 sm:w-fit'/>
             </div>
         </div>
     );
